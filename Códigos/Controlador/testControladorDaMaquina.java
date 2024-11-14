c197ba22e9ee078d547b01a2276478130084e218
Guilherme Jaques <guilhermejaques.aluno@unipampa.edu.br>
2024-11-14 18:21:20 +0000

    Upload New File

---
+++ Códigos/Controlador/testControladorDaMaquina.java
@@ -0,0 +1,47 @@
+
+import org.junit.Test;
+
+public class testControladorDaMaquina{
+    
+    @Test
+    public void controlar(){
+        
+    }
+    
+    public void fluxoPrinciapal(){
+        // sistema pronto para uso e mostra mensagem no display
+        
+        // compadror deposita uma moeda
+        
+        // sistema registra e mostra no display o valor acumulado
+        
+        // comprador escolhe bebida
+        
+        // sistema recolhe as moedas da entrada do cofre, libera a bebida escolhida e destrava o dispenser de bebida
+        
+        // sistema calcula troco, recupera moedas do reservatorio do cofre e libera troco
+        
+        // comprador abre a portinha, pega a bebida e fecha a portinha
+        
+        // comprador pega troco no dispenser de dinheiro
+        
+        // sistema retorna ao estado de pronto
+        
+    }
+    
+    public void fluxoAlternativo (){
+        // sistema pronto para uso e mostra mensagem no display
+        
+        // compadror deposita uma moeda
+        
+        // sistema registra e mostra no display o valor acumulado
+        
+        // comprador escolhe receber de volta as moedas
+        
+        // sistema envia medas da entrada para dispenser de troco
+        
+        // sistema retorna ao estado de pronto
+        
+    }
+    
+}
