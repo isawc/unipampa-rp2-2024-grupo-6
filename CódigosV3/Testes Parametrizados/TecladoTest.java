0152f160c2d0e2309f76afa171f3c85d5f40072d
Isaac Nilton Alvarenga da Silva <isaacnasilva@gmail.com>
2024-12-02 22:30:52 +0000

    Update TecladoTest.java

--- CódigosV3/Testes Parametrizados/TecladoTest.java
+++ CódigosV3/Testes Parametrizados/TecladoTest.java
@@ -1,128 +1,57 @@
 import io.Teclado;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+//parametrizado (isaac)
+@RunWith(Parameterized.class)
 public class TecladoTest {
+
     private Teclado teclado;
     private NotificacaoNew notificavel;
+
+    @Parameterized.Parameter(0)
+    public int botaoPressionado;
+
+    @Parameterized.Parameter(1)
+    public int valorEsperado;
 
     @Before
     public void inicializar() {
         teclado = new Teclado();
-        notificavel = new NotificacaoNew();
+        notificavel = new NotificacaoNew(); // Substituído futuramente pelo controlador
     }
-
-    
-    //mudar valoresperado
-    @Test
-    public void testApertarBotaoCocaCola() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_COCA_COLA);
-        assertEquals(Teclado.BOTAO_COCA_COLA, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 37;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoCocaColaZero() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_COCA_ZERO);
-        assertEquals(Teclado.BOTAO_COCA_ZERO, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 3;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoCocaColaLight() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_COCA_LIGHT);
-        assertEquals(Teclado.BOTAO_COCA_LIGHT, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 5;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
+    //parametrizados (isaac)
+    @Parameterized.Parameters(name = "{index}: Botão pressionado={0}, Valor esperado={1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {Teclado.BOTAO_COCA_COLA, 37},
+            {Teclado.BOTAO_COCA_ZERO, 3},
+            {Teclado.BOTAO_COCA_LIGHT, 5},
+            {Teclado.BOTAO_SPRITE, 7},
+            {Teclado.BOTAO_SPRITE_ZERO, 11},
+            {Teclado.BOTAO_FANTA_LARANJA, 13},
+            {Teclado.BOTAO_FANTA_UVA, 17},
+            {Teclado.BOTAO_KUAT_GUARANA, 19},
+            {Teclado.BOTAO_SCHWEPPES, 23},
+            {Teclado.BOTAO_SCHWEPPES_CITRUS, 29},
+            {Teclado.BOTAO_DEVOLVER_DINHEIRO, 31},
+            {35, 35}, // Botão inválido
+            {-1, Teclado.NENHUM_BOTAO_PRESSIONADO} // Nenhum botão pressionado
+        });
     }
 
     @Test
-    public void testApertarBotaoSprite() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_SPRITE);
-        assertEquals(Teclado.BOTAO_SPRITE, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 7;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoSpriteZero() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_SPRITE_ZERO);
-        assertEquals(Teclado.BOTAO_SPRITE_ZERO, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 11;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoFantaLaranja() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_FANTA_LARANJA);
-        assertEquals(Teclado.BOTAO_FANTA_LARANJA, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 13;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoFantaUva() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_FANTA_UVA);
-        assertEquals(Teclado.BOTAO_FANTA_UVA, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 17;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoKuatGuarana() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_KUAT_GUARANA);
-        assertEquals(Teclado.BOTAO_KUAT_GUARANA, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 19;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoSchweppes() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_SCHWEPPES);
-        assertEquals(Teclado.BOTAO_SCHWEPPES, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 23;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoSchweppesCitrus() {
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_SCHWEPPES_CITRUS);
-        assertEquals(Teclado.BOTAO_SCHWEPPES_CITRUS, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 29;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testApertarBotaoInvalido() {
-        int botaoInvalido = 35;
-        teclado.simulaApertarBotao(notificavel, botaoInvalido);
-        assertEquals(35, teclado.getUltimoBotaoPressionado());
-        
-    }
-
-    @Test
-    public void testApertarCombinacaoDeBotoes() {
-        int combinacaoEsperada = Teclado.BOTAO_COCA_COLA * 103 * Teclado.BOTAO_COCA_LIGHT * Teclado.BOTAO_SPRITE;
-
-        teclado.simulaApertarBotoes(notificavel, Teclado.BOTAO_COCA_COLA, Teclado.BOTAO_COCA_LIGHT, Teclado.BOTAO_SPRITE);
-        assertEquals(combinacaoEsperada, teclado.getUltimoBotaoPressionado());
-    }
-
-    @Test
-    public void testNenhumBotaoPressionado() {
-        assertEquals(Teclado.NENHUM_BOTAO_PRESSIONADO, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = -1;
+    public void testApertarBotao() {
+        teclado.simulaApertarBotao(notificavel, botaoPressionado);
+        assertEquals(botaoPressionado, teclado.getUltimoBotaoPressionado());
         assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
     }
     
-    @Test
-    public void testApertarDevolverDinheiro(){
-        teclado.simulaApertarBotao(notificavel, Teclado.BOTAO_DEVOLVER_DINHEIRO);        
-        assertEquals(Teclado.BOTAO_DEVOLVER_DINHEIRO, teclado.getUltimoBotaoPressionado());
-        int valorEsperado = 31;
-        assertEquals(valorEsperado, teclado.getUltimoBotaoPressionado());
-    }
+    
 }
